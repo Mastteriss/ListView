dcee647406c0cb924a4e3b117e782c4bdca97ce2
Mastteriss <75664149+Mastteriss@users.noreply.github.com>
2024-12-07 19:12:26 +0300

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.list_view
+
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    val entry:MutableList<String> = mutableListOf()
+
+    private lateinit var  editTextName:EditText
+    private lateinit var editTextAge:EditText
+    private lateinit var buttonSaveBTN:Button
+    private lateinit var buttonExitBTN:Button
+    private lateinit var listView: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        editTextAge = findViewById(R.id.editTextAge)
+        editTextName = findViewById(R.id.editTextName)
+        buttonExitBTN = findViewById(R.id.buttonExitBTN)
+        buttonSaveBTN = findViewById(R.id.buttonSaveBTN)
+        listView = findViewById(R.id.listView)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, entry )
+        listView.adapter = adapter
+
+        buttonSaveBTN.setOnClickListener {
+            val name = editTextName.text.toString()
+            val age = editTextAge.text.toString()
+            if(name.isNotEmpty() && age.isNotEmpty()){
+                entry.add("$name, $age")
+                adapter.notifyDataSetChanged()
+
+                editTextName.text.clear()
+                editTextAge.text.clear()
+            }
+        }
+        listView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, v, position, id ->
+                val note = adapter.getItem(position)
+                adapter.remove(note)
+                Toast.makeText(this, "Пользователь удален: $note", Toast.LENGTH_LONG).show()
+            }
+
+        buttonExitBTN.setOnClickListener {
+            finish()
+        }
+    }
+}
